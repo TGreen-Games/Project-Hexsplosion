13c5044d3329d56ff5e1dc0fb5e6c81429af15b9
Warren Green <trey@warrens-mbp.home>
2017-10-13 00:15:49 -0500

    Project Saved

---
+++ Prototype V1.0/Assets/Scripts/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour {
+    private Color playerColor;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D player)
+    {
+        if (player.gameObject.CompareTag("Player"))
+            playerColor = player.GetComponent<SpriteRenderer>().color;
+            if (player.GetComponent<FillShape>().CanCapture)
+            {
+                Debug.Log("TRIGGERED!!!!!");
+                this.GetComponent<Image>().color = playerColor;
+            }
+            
+        }
+    
+}
